b25e8da8cec2661da37021faa1e8490748d77ea5
javad7721 <m.javad7721@gmail.com>
2023-06-20 11:56:31 +0330

    add Component charCount

---
+++ src/Components/CharacterCountComponent.jsx
@@ -0,0 +1,7 @@
+
+const CharacterCountComponent = ({ inputValue }) => {
+    return (
+        <p>تعداد کاراکترهای تایپ شده: {inputValue.length}</p>
+    );
+};
+export default CharacterCountComponent;
